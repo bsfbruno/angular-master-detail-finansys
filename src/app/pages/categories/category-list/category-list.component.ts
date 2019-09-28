6b56ecefcfd1089f37c265d1af448b3e55cb8423
Bruno <brunosuportefs@gmail.com>
2019-09-28 10:01:01 -0300

    Listando e Deletendo em Category-List

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,3 +1,5 @@
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/category.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      response => this.categories = response,
+      error => alert('Erro ao carregar Lista')
+    );
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja excluir este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        //retorna os valores atualizados do array que sejam diferentes da categoria recebida por parâmetro
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert('Erro ao excluir!')
+      );
+    }
+    
   }
 
 }
