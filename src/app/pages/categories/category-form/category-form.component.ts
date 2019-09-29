89dd10b039a4a4ab01f439369236463d992950ca
Bruno <brunosuportefs@gmail.com>
2019-09-29 11:46:11 -0300

    Adicionando metodos ao Category-Form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/category.model';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
+  
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submmitingForm: boolean = false;
+  category: Category = new Category();
 
-  constructor() { }
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
+  }
+  
+  ngAfterContentChecked(): void {
+    this.setPageTitle();
+  }
+
+  private setCurrentAction() {
+    if (this.route.snapshot.url[0].path == "new")
+      this.currentAction = "new"
+    else
+      this.currentAction = "edit"
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if (this.currentAction == "edit") {
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get("id")))
+      )
+      .subscribe(
+        (response) => {
+          this.category = response;
+          this.categoryForm.patchValue(this.category)
+        },
+        (error) => alert('Ocorreu um erro no Servidor')
+      );
+    }
+  }
+
+  private setPageTitle() {
+    if (this.currentAction == "new") {
+      this.pageTitle = "Cadastro de Nova Categoria"
+    } else {
+      //ao carregar, por um segundo, o category recebe null. O comando serve para evitar aparecer null no título.
+      const categoriaName = this.category.name || ""
+      this.pageTitle = "Editando Categoria: " + this.category.name;
+    }
   }
 
 }
