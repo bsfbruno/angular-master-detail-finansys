f4b07c1d18436bb270a86e75d1d65ba6709bb617
Bruno <brunosuportefs@gmail.com>
2019-10-21 21:15:14 -0300

    Adicionando o formulario de Receitas e Despesas

---
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -0,0 +1,131 @@
+import { EntryService } from './../shared/entry.service';
+import { Entry } from './../shared/entry.model';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import * as toastr from "toastr";
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.css']
+})
+export class EntryFormComponent implements OnInit, AfterContentChecked {
+  
+  currentAction: string;
+  entryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submmitingForm: boolean = false;
+  entry: Entry = new Entry();
+
+  constructor(
+    private entryService: EntryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.setCurrentAction();
+    this.buildEntryForm();
+    this.loadEntry();
+  }
+  
+  ngAfterContentChecked(): void {
+    this.setPageTitle();
+  }
+
+  public submitForm() {
+    this.submmitingForm = true;
+
+    if (this.currentAction == "true")
+      this.createEntry();
+    else
+      this.updateEntry();
+  }
+
+  private setCurrentAction() {
+    if (this.route.snapshot.url[0].path == "new")
+      this.currentAction = "new"
+    else
+      this.currentAction = "edit"
+  }
+
+  private buildEntryForm() {
+    this.entryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]]
+    });
+  }
+
+  private loadEntry() {
+    if (this.currentAction == "edit") {
+      this.route.paramMap.pipe(
+        switchMap(params => this.entryService.getById(+params.get("id")))
+      )
+      .subscribe(
+        (response) => {
+          this.entry = response;
+          this.entryForm.patchValue(this.entry)
+        },
+        (error) => alert('Ocorreu um erro no Servidor')
+      );
+    }
+  }
+
+  private setPageTitle() {
+    if (this.currentAction == "new") {
+      this.pageTitle = "Cadastro de Novo Lançamento"
+    } else {
+      //ao carregar, por um segundo, o entry recebe null. O comando serve para evitar aparecer null no título.
+      const entryName = this.entry.name || ""
+      this.pageTitle = "Editando Lançamento: " + this.entry.name;
+    }
+  }
+
+  private createEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.create(entry).subscribe(
+      response => {
+        this.actionsForSuccess(response);
+      },
+      error => {
+        this.actionsForError(error);
+      }
+    );
+  }
+
+  private updateEntry() {
+
+  }
+
+  private actionsForSuccess(entry: Entry) {
+    toastr.success("Solicitação processada com sucesso!");
+
+    this.router.navigateByUrl("entries", {skipLocationChange: true}).then(
+      () => this.router.navigate(["entries", entry.id, "edit"])
+    );
+  }
+
+  private actionsForError(error) {
+    toastr.error("Ocorreu um erro ao processar a sua solicitação!");
+
+    this.submmitingForm = false;
+
+    if (error.status === 422)
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    else
+      this.serverErrorMessages = ["Falha na comunicação com o servidor"];
+  }
+
+}
